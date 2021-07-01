ef724b34fb45b0f01a0bfed537f6ca120681d9b4
prashanth-sams <sams.prashanth@gmail.com>
2021-07-01 14:57:31 +0400

    first commit

---
+++ src/test/java/com/wmock/demo/WiremockApiTests.java
@@ -0,0 +1,13 @@
+package com.wmock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
