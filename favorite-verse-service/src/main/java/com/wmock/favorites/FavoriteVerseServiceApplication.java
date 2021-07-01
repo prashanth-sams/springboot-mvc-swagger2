999637a9b81ec8b3441b65ba391fa32596b089c6
prashanth-sams <sams.prashanth@gmail.com>
2021-07-01 16:30:16 +0400

    first commit

---
+++ favorite-verse-service/src/main/java/com/wmock/favorites/FavoriteVerseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wmock.favorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteVerseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteVerseServiceApplication.class, args);
+	}
+
+}
---
+++ favorite-verse-service/src/test/java/com/wmock/favorites/FavoriteVerseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmock.favorites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteVerseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
