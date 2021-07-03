2bf3cbc5ff915da518dd67857074f2818de8b4dc
prashanth-sams <sams.prashanth@gmail.com>
2021-07-03 21:19:44 +0400

    integration with external microservices - manual

---
+++ chapter-catalog-service/src/main/java/com/wmock/catalog/models/ChapterInfoModel.java
@@ -0,0 +1,31 @@
+package com.wmock.catalog.models;
+
+public class ChapterInfoModel {
+
+    private String chapterId;
+    private String name;
+
+    public ChapterInfoModel() {
+    }
+
+    public ChapterInfoModel(String chapterId, String name) {
+        this.chapterId = chapterId;
+        this.name = name;
+    }
+
+    public String getChapterId() {
+        return chapterId;
+    }
+
+    public void setChapterId(String chapterId) {
+        this.chapterId = chapterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ chapter-catalog-service/src/main/java/com/wmock/catalog/models/NotesModel.java
@@ -0,0 +1,32 @@
+package com.wmock.catalog.models;
+
+public class NotesModel {
+
+    private String chapterId;
+    private int notes;
+
+    public NotesModel() {
+    }
+
+    public NotesModel(String chapterId, int notes) {
+        this.chapterId = chapterId;
+        this.notes = notes;
+    }
+
+    public String getChapterId() {
+        return chapterId;
+    }
+
+    public void setChapterId(String chapterId) {
+        this.chapterId = chapterId;
+    }
+
+    public int getNotes() {
+        return notes;
+    }
+
+    public void setNotes(int notes) {
+        this.notes = notes;
+    }
+
+}
